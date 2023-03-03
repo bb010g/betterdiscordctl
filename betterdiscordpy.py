a40512af4fbd4736a69e17655ed14b7114acc2d0
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2023-03-03 08:27:11 +0100

    added more commands

--- betterdiscordpy.py
+++ betterdiscordpy.py
@@ -11,9 +11,21 @@ self_upgrade_url = 'https://github.com/bb010g/betterdiscordctl/raw/master/better
 
 parser = argparse.ArgumentParser(description="Manage BetterDiscord installations on Linux")
 parser.add_argument("-V", "--version", action='store_true', help="display version info and exit")
+parser.add_argument("command", choices=["status", "install", "reinstall", "uninstall", "self-upgrade"])
 args = parser.parse_args()
 
 if args.version:
     print(pname + " " + version)
+#elif args.command:
+    if args.command == "status":
+        print("test")
+    if args.command == "install":
+        print("test1")
+    if args.command == "reinstall":
+        print("test2")
+    if args.command == "uninstall":
+        print("test3")
+    if args.command == "self-upgrade":
+        print("test4")
 else:
     parser.print_help()
