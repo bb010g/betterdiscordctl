a58b446ef191ba0db79f2056152c717a0a1a47b4
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2023-03-17 08:23:36 +0100

    adding stuff

--- betterdiscordpy.py
+++ betterdiscordpy.py
@@ -1,8 +1,9 @@
-import argparse
+import argparse, os
 
 pname = "betterdiscordctl"
 # Constants
 version = "2.0.1"
+verbosity = 0
 
 # Options
 verbosity = 0
@@ -12,7 +13,22 @@ self_upgrade_url = 'https://github.com/bb010g/betterdiscordctl/raw/master/better
 parser = argparse.ArgumentParser(description="Manage BetterDiscord installations on Linux")
 parser.add_argument("-V", "--version", action='store_true', help="display version info and exit")
 parser.add_argument("command", choices=["status", "install", "reinstall", "uninstall", "self-upgrade"])
+parser.add_argument('-v', "--verbose", action='store_true', help="increase verbosity")
+parser.add_argument('-q', "--quiet", action='store_true', help="decrease verbosity")
 args = parser.parse_args()
+
+def verbose(vmsg):
+    global verbosity
+    if verbosity > 0:
+        return True
+        if not vmsg == str:
+            print("InvalidStringError: The input is not a valid string")
+        elif vmsg == str:
+            print(vmsg)
+        
+def xdg_discover_config():
+    if d_install == "traditional":
+            
 
 if args.version:
     print(pname + " " + version)
@@ -20,12 +36,16 @@ elif args.command:
     if args.command == "status":
         print("test")
     if args.command == "install":
-        print("test1")
+        
     if args.command == "reinstall":
         print("test2")
     if args.command == "uninstall":
         print("test3")
     if args.command == "self-upgrade":
         print("test4")
+elif args.verbose:
+    verbosity =+ 1
+elif args.quiet:
+    verbosity = 0
 else:
     parser.print_help()
