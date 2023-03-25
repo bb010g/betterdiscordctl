02a2b613ee07acb84c9e1ba4c476c76cbdd65fda
skelly37 <skelly37@protonmail.com>
2023-03-25 13:07:45 +0100

    initial log is private as it should be

--- commands.py
+++ commands.py
@@ -2,29 +2,29 @@ import argparse
 import logging
 
 
-def initialLog(command_name: str, arguments: argparse.Namespace) -> None:
+def __initialLog(command_name: str, arguments: argparse.Namespace) -> None:
     logging.log(logging.DEBUG, f"Running command: {command_name}")
     logging.log(logging.DEBUG, f"Received args: {arguments}")
 
 
 def status(arguments: argparse.Namespace) -> None:
-    initialLog(command_name="status", arguments=arguments)
+    __initialLog(command_name="status", arguments=arguments)
 
 
 def install(arguments: argparse.Namespace) -> None:
-    initialLog(command_name="install", arguments=arguments)
+    __initialLog(command_name="install", arguments=arguments)
 
 
 def reinstall(arguments: argparse.Namespace) -> None:
-    initialLog(command_name="reinstall", arguments=arguments)
+    __initialLog(command_name="reinstall", arguments=arguments)
 
 
 def uninstall(arguments: argparse.Namespace) -> None:
-    initialLog(command_name="uninstall", arguments=arguments)
+    __initialLog(command_name="uninstall", arguments=arguments)
 
 
 def selfUpgrade(arguments: argparse.Namespace) -> None:
-    initialLog(command_name="self-upgrade", arguments=arguments)
+    __initialLog(command_name="self-upgrade", arguments=arguments)
 
 
 AVAILABLE_COMMANDS: dict[str, callable] = {
